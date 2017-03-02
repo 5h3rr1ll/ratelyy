fb3127bc2e0d24bdd9b9616abeb1aab1841a02ce
5h3rr1ll <a@sherrill.de>
2017-03-02 15:40:06 +0100

    Modul angefangen um Benutzer hinzuzufügen

--- webCrawler.py
+++ webCrawler.py
@@ -3,7 +3,7 @@
 # @Email:  jigg4@me.com
 # @Filename: webCrawler.py
 # @Last modified by:   jiGg4
-# @Last modified time: 01-03-2017
+# @Last modified time: 02-03-2017
 
 
 
@@ -13,14 +13,21 @@
 from urllib import request, parse
 import re, sys
 
-url = "https:" + parse.quote("//de.wikipedia.org/wiki/Liste_von_Nestlé-Marken")
+from urllib.parse import urlparse
+
+o = urlparse("https://de.wikipedia.org/wiki/Liste_von_Nestlé-Marken")
+print(o.geturl())
+site = request.urlopen(o.geturl())
+html = site.read().decode()
+print(html)
+"""url = "https:" + parse.quote("//de.wikipedia.org/wiki/Liste_von_Nestlé-Marken")
 
 site = request.urlopen(url)
 
 html = site.read().decode()
 
-regO = re.findall("http[a]*:\/\/\w*\.\w{1,3}\/*\w*\W\w*\W\w*\W\w*\W\w*\W\w*\W", html)
+regex = r"http[a]*:\/\/\w*\.\w{1,3}\/*\w*\W\w*\W\w*\W\w*\W\w*\W\w*\W"
 
-print(regO)
-#for i in regO:
-#    print(i,"\n")
+regO = re.findall(regex, html)
+
+print(regO)"""
